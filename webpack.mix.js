ae56b52bbf7c82172d5bb3cded4fff8860b1da6d
paranarimasu <33796518+paranarimasu@users.noreply.github.com>
2017-12-19 22:36:06 -0600

    Add custom modernizr build in webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,18 @@
 let mix = require('laravel-mix');
 
 mix.copy('node_modules/jquery/dist/jquery.min.js', 'public/js/vendor/jquery.min.js');
+
+var modernizr = require("modernizr");
+
+modernizr.build({
+    "options": [
+        "setClasses"
+    ],
+    "feature-detects": [
+        "video"
+    ]
+}, function (result) {
+    var modernizrJS = new File('public/js/vendor/modernizr.min.js');
+    modernizrJS.write(result);
+    modernizrJS.minify();
+});
