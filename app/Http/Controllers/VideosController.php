0d47f31fe6fc773dbb9217d14d68d374455706b4
paranarimasu <33796518+paranarimasu@users.noreply.github.com>
2017-12-28 18:28:46 -0700

    Fixes #1 - Return file response directly from storage

--- app/Http/Controllers/VideosController.php
+++ app/Http/Controllers/VideosController.php
@@ -17,30 +17,11 @@ class VideosController extends Controller
         ]);
     }
 
-    // Source: https://stackoverflow.com/q/36778167
     public function show($alias) {
         set_time_limit(0);
 
         $video = Video::where('alias', $alias)->firstOrFail();
 
-        $fs = Storage::disk('spaces')->getDriver();
-
-        $metaData = $fs->getMetadata($video->path);
-        $stream = $fs->readStream($video->path);
-
-        if (ob_get_level()) {
-            ob_end_clean();
-        }
-
-        $headers = [
-            'Content-Type' => $metaData['type'],
-            'Accept-Ranges' => 'bytes',
-            'Content-Length' => $metaData['size'],
-            'Content-Range' => 'bytes 0-' . ($metaData['size'] - 1) . '/' . $metaData['size'],
-        ];
-
-        return response()->stream(function () use ($stream) {
-            fpassthru($stream);
-        }, 206, $headers);
+        return Storage::disk('spaces')->response($video->path);
     }
 }
