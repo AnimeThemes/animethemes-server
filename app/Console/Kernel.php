c7b99fdce2ed8c33b4a4f142446d15472dd9a12a
paranarimasu <33796518+paranarimasu@users.noreply.github.com>
2018-01-06 23:54:31 -0600

    Fixing Scheduled Task

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\SyncVideosCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,7 +14,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        Commands\SyncVideosCommand::class
+        \App\Console\Commands\SyncVideosCommand::class
     ];
 
     /**
