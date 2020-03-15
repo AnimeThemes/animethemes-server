f0f7bdcbfae11cbedcb1bba2491c0436c0716f60
paranarimasu <33796518+paranarimasu@users.noreply.github.com>
2020-03-15 18:42:18 -0500

    Maintenance [Update dependencies / Adding Discord Invite Link]

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -4,6 +4,7 @@
         <li><a href="https://www.reddit.com/r/AnimeThemes/">Reddit</a></li>
         <li><a href="https://twitter.com/parameterized">Twitter</a></li>
         <li><a href="https://github.com/paranarimasu/AnimeThemes">Github</a></li>
+        <li><a href="https://discordapp.com/invite/m9zbVyQ">Discord</a></li>
         <li><a href="https://old.reddit.com/r/AnimeThemes/wiki/index">Browse</a></li>
     </ul>
 </nav>
