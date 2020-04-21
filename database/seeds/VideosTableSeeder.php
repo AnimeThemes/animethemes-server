768ab792ab8d642c388675f84f226dc79015e5aa
paranarimasu <33796518+paranarimasu@users.noreply.github.com>
2020-04-20 23:49:09 -0500

    VideoTableSeeder cleanup

--- database/seeds/VideosTableSeeder.php
+++ database/seeds/VideosTableSeeder.php
@@ -8,14 +8,18 @@ class VideosTableSeeder extends Seeder
 {
 
     public function run() {
+        // Remove any existing rows in Videos table
+        // We want this table to match storage, avoiding the need for reconciliation
         DB::table('videos')->delete();
 
+        // Get metadata for all objects in storage
         $fs = Storage::disk('spaces');
         $files = $fs->listContents('', true);
 
+        // Create videos from WebM metadata
+        // Bulk insertion violates default packet size constraints
         foreach ($files as $file) {
-            $isFile = $file['type'] == 'file';
-            if ($isFile) {
+            if ($file['type'] === 'file' && $file['extension'] === 'webm') {
                 Video::create(array(
                     'basename' => $file['basename'],
                     'filename' => $file['filename'],
