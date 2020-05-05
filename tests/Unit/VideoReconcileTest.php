eb170818a3b2442a8bb0567be67f34212e12495b
paranarimasu <33796518+paranarimasu@users.noreply.github.com>
2020-05-04 23:05:59 -0500

    test coverage & updating dependencies

--- tests/Unit/VideoReconcileTest.php
+++ tests/Unit/VideoReconcileTest.php
@@ -3,85 +3,103 @@
 namespace Tests\Unit;
 
 use App\Console\Commands\VideoReconcileCommand;
+use App\Models\Video;
+use Illuminate\Support\Str;
+use Mockery;
 use PHPUnit\Framework\TestCase;
 
 class VideoReconcileTest extends TestCase
 {
 
+    protected $command;
+    protected $video_a;
+    protected $video_b;
+
+    protected function setUp(): void {
+        parent::setUp();
+        $this->command = new VideoReconcileCommand;
+        $this->video_a = Mockery::mock(Video::class)->makePartial();
+        $this->video_b = Mockery::mock(Video::class)->makePartial();
+    }
+
     public function testNoResults() {
-        $command = new VideoReconcileCommand;
-        $this->assertFalse($command->hasResults());
+        $this->assertFalse($this->command->hasResults());
     }
 
     public function testNoResultsVideoCreated() {
-        $command = new VideoReconcileCommand;
-        $command->created++;
-        $this->assertTrue($command->hasResults());
+        $this->command->created++;
+        $this->assertTrue($this->command->hasResults());
     }
 
     public function testNoResultsVideoCreatedFailed() {
-        $command = new VideoReconcileCommand;
-        $command->created_failed++;
-        $this->assertTrue($command->hasResults());
+        $this->command->created_failed++;
+        $this->assertTrue($this->command->hasResults());
     }
 
     public function testNoResultsVideoDeleted() {
-        $command = new VideoReconcileCommand;
-        $command->deleted++;
-        $this->assertTrue($command->hasResults());
+        $this->command->deleted++;
+        $this->assertTrue($this->command->hasResults());
     }
 
     public function testNoResultsVideoDeletedFailed() {
-        $command = new VideoReconcileCommand;
-        $command->deleted_failed++;
-        $this->assertTrue($command->hasResults());
+        $this->command->deleted_failed++;
+        $this->assertTrue($this->command->hasResults());
     }
 
     public function testHasChanges() {
-        $command = new VideoReconcileCommand;
-        $this->assertFalse($command->hasChanges());
+        $this->assertFalse($this->command->hasChanges());
     }
 
     public function testHasChangesFailed() {
-        $command = new VideoReconcileCommand;
-        $command->deleted_failed++;
-        $command->created_failed++;
-        $this->assertFalse($command->hasChanges());
+        $this->command->deleted_failed++;
+        $this->command->created_failed++;
+        $this->assertFalse($this->command->hasChanges());
     }
 
     public function testHasChangesCreated() {
-        $command = new VideoReconcileCommand;
-        $command->created++;
-        $this->assertTrue($command->hasChanges());
+        $this->command->created++;
+        $this->assertTrue($this->command->hasChanges());
     }
 
     public function testHasChangesDeleted() {
-        $command = new VideoReconcileCommand;
-        $command->deleted++;
-        $this->assertTrue($command->hasChanges());
+        $this->command->deleted++;
+        $this->assertTrue($this->command->hasChanges());
     }
 
     public function testHasFailures() {
-        $command = new VideoReconcileCommand;
-        $this->assertFalse($command->hasFailures());
+        $this->assertFalse($this->command->hasFailures());
     }
 
     public function testHasFailuresSuccess() {
-        $command = new VideoReconcileCommand;
-        $command->created++;
-        $command->deleted++;
-        $this->assertFalse($command->hasFailures());
+        $this->command->created++;
+        $this->command->deleted++;
+        $this->assertFalse($this->command->hasFailures());
     }
 
     public function testHasFailuresCreated() {
-        $command = new VideoReconcileCommand;
-        $command->created_failed++;
-        $this->assertTrue($command->hasFailures());
+        $this->command->created_failed++;
+        $this->assertTrue($this->command->hasFailures());
     }
 
     public function testHasFailuresDeleted() {
-        $command = new VideoReconcileCommand;
-        $command->deleted_failed++;
-        $this->assertTrue($command->hasFailures());
+        $this->command->deleted_failed++;
+        $this->assertTrue($this->command->hasFailures());
+    }
+
+    public function testReconciliationString() {
+        $this->assertEquals(VideoReconcileCommand::reconciliationString($this->video_a), "basename:{$this->video_a->basename},filename:{$this->video_a->filename},path:{$this->video_a->path}");
+    }
+
+    public function testCompareVideosEquals() {
+        $this->assertEquals(VideoReconcileCommand::compareVideos($this->video_a, $this->video_a), 0);
+    }
+
+    public function testCompareVideosNotEquals() {
+        $this->assertEquals(VideoReconcileCommand::compareVideos($this->video_a, $this->video_b), 0);
+    }
+
+    public function tearDown(): void {
+        parent::tearDown();
+        Mockery::close();
     }
 }
