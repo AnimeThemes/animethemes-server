78314118f2dc56ffeb60a6e214ef12b19b51a2e3
paranarimasu <33796518+paranarimasu@users.noreply.github.com>
2020-05-17 16:55:52 -0500

    Nova - attach anime when seeding series

--- database/seeds/SeriesSeeder.php
+++ database/seeds/SeriesSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Anime;
 use App\Models\Series;
 use Illuminate\Database\Seeder;
 
@@ -12,26 +13,48 @@ class SeriesSeeder extends Seeder
      */
     public function run()
     {
+        // Remove any existing rows in Series-related tables
+        // We want these tables to match the subreddit wiki
         DB::table('anime_series')->delete();
         DB::table('series')->delete();
 
+        // Get JSON of Series Index page content
         $series_wiki_contents = file_get_contents('https://old.reddit.com/r/AnimeThemes/wiki/series.json');
         $series_wiki_json = json_decode($series_wiki_contents);
         $series_wiki_content_md = $series_wiki_json->data->content_md;
 
+        // Match Series Entries
+        // Format: "[{Series Name}](/r/AnimeThemes/wiki/series/{Series Alias}/)"
         preg_match_all('/\[.*\]\(.*series.*\)/m', $series_wiki_content_md, $series_wiki_entries, PREG_PATTERN_ORDER);
 
         foreach ($series_wiki_entries[0] as $series_wiki_entry) {
+
+            // Get Series Name, Series Alias, and Series Link from Entry
             preg_match('/\[(.*)\]/', $series_wiki_entry, $series_wiki_entry_name);
             preg_match('/\[.*]\(\/r\/AnimeThemes\/wiki\/series\/(.*)\)/', $series_wiki_entry, $series_wiki_entry_alias);
-            //TODO: match link
+            preg_match('/\[.*]\((\/r\/AnimeThemes\/wiki\/series\/.*)\)/', $series_wiki_entry, $series_wiki_entry_link);
 
-            Series::create([
+            // Create Model from subreddit Alias & Name
+            $series = Series::create([
                 'name' => $series_wiki_entry_name[1],
                 'alias' => $series_wiki_entry_alias[1]
             ]);
 
-            //TODO: visit matched link, match anime headers, link anime by name
+            // Try not to upset Reddit
+            sleep(rand(5, 15));
+
+            // Get JSON of Series Entry page content
+            $series_anime_wiki_contents = file_get_contents('https://old.reddit.com' . $series_wiki_entry_link[1] . '.json');
+            $series_anime_wiki_json = json_decode($series_anime_wiki_contents);
+            $series_anime_wiki_content_md = $series_anime_wiki_json->data->content_md;
+
+            // Match headers of Anime that belong to the Series
+            // Format: "###[{Anime Name}]({Resource Link})
+            preg_match_all('/###\[(.*)\]\(.*\)/m', $series_anime_wiki_content_md, $series_anime_wiki_entries, PREG_PATTERN_ORDER);
+
+            // Attach Anime to Series by Name
+            $series_anime = Anime::whereIn('name', $series_anime_wiki_entries[1])->get();
+            $series->anime()->sync($series_anime);
         }
     }
 }
