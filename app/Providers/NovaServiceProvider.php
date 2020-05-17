888ba91e0a7be50a05f790893cf9bfb3e09a866b
paranarimasu <33796518+paranarimasu@users.noreply.github.com>
2020-05-16 19:28:04 -0500

    nova - more staging deployment fixes

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -42,9 +42,7 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     protected function gate()
     {
         Gate::define('viewNova', function ($user) {
-            return in_array($user->email, [
-                //
-            ]);
+            return in_array($user->email, explode(',', env('NOVA_VIEW_LIST')));
         });
     }
 
