b1122e62d7338593928ecaa514ccbd2d26453504
paranarimasu <33796518+paranarimasu@users.noreply.github.com>
2020-12-11 16:28:10 -0600

    chore: fixing nav links

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -2,9 +2,11 @@
     <ul>
         <li><a href="https://www.reddit.com/r/AnimeThemes/">Reddit</a></li>
         <li><a href="https://discordapp.com/invite/m9zbVyQ">Discord</a></li>
-        <li><a href="https://github.com/paranarimasu/AnimeThemes">Github</a></li>
-        <li><a href="https://twitter.com/parameterized">Twitter</a></li>
+        <li><a href="https://github.com/AnimeThemes/animethemes-server">Github</a></li>
+        <li><a href="https://twitter.com/AnimeThemesMoe">Twitter</a></li>
         <li><a href="https://www.patreon.com/AnimeThemes">Patreon</a></li>
+        <li><a href="https://ko-fi.com/animethemes">Ko-fi</a></li>
         <li><a href="https://old.reddit.com/r/AnimeThemes/wiki/index">Browse</a></li>
+        <li><a href="https://staging.animethemes.moe/">Staging</a></li>
     </ul>
 </nav>
