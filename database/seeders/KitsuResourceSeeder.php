0934a335b6a45bc376e9d3d91fda37f4d0d2ef26
paranarimasu <33796518+paranarimasu@users.noreply.github.com>
2021-01-15 23:12:55 -0600

    #45 feat(seed): create kitsu anime resources (#100)
    * #45 feat(seed): seed kitsu anime resource
    
    * style: fix StyleCI findings

---
+++ database/seeders/KitsuResourceSeeder.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Enums\ResourceSite;
+use App\Models\Anime;
+use App\Models\ExternalResource;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\ServerException;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+class KitsuResourceSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Get anime that have MAL resource but do not have Kitsu resource
+        $animes = Anime::whereHas('externalResources', function ($resource_query) {
+            $resource_query->where('site', ResourceSite::MAL);
+        })->whereDoesntHave('externalResources', function ($resource_query) {
+            $resource_query->where('site', ResourceSite::KITSU);
+        })
+        ->get();
+
+        foreach ($animes as $anime) {
+            $mal_resource = $anime->externalResources->firstWhere('site', strval(ResourceSite::MAL));
+            if (! is_null(optional($mal_resource)->external_id)) {
+
+                // Try not to upset Kitsu
+                sleep(rand(5, 15));
+
+                // Kitsu api call
+                try {
+                    $client = new Client;
+                    $response = $client->get('https://kitsu.io/api/edge/mappings?filter[externalSite]=myanimelist/anime&include=item&filter[externalId]='.$mal_resource->external_id, [
+                        'headers' => [
+                            'Accept' => 'application/vnd.api+json',
+                            'Content-Type' => 'application/vnd.api+json',
+                        ],
+                    ]);
+                    $kitsu_resource_json = json_decode($response->getBody()->getContents());
+
+                    $kitsu_resource_data = property_exists($kitsu_resource_json, 'data') ? $kitsu_resource_json->data : [];
+                    $kitsu_resource_included = property_exists($kitsu_resource_json, 'included') ? $kitsu_resource_json->included : [];
+
+                    // Only proceed if we have a single match
+                    if (count($kitsu_resource_data) == 1 && count($kitsu_resource_included) == 1) {
+                        $kitsu_id = $kitsu_resource_included[0]->id;
+                        $kitsu_slug = $kitsu_resource_included[0]->attributes->slug;
+
+                        // Check if Kitsu resource already exists
+                        $kitsu_resource = ExternalResource::where('site', ResourceSite::KITSU)->where('external_id', $kitsu_id)->first();
+
+                        // Create Kitsu resource if it doesn't already exist
+                        if (is_null($kitsu_resource)) {
+                            $kitsu_resource = ExternalResource::create([
+                                'site' => ResourceSite::KITSU,
+                                'link' => "https://kitsu.io/anime/{$kitsu_slug}",
+                                'external_id' => $kitsu_id,
+                            ]);
+                        }
+
+                        // Attach Kitsu resource to anime
+                        $kitsu_resource->anime()->syncWithoutDetaching($anime);
+                    }
+                } catch (HttpException $e) {
+                    // There was some issue with the request
+                    Log::info($e->getMessage());
+                } catch (ClientException $e) {
+                    // We may not have a match for this MAL resource
+                    Log::info($e->getMessage());
+                } catch (ServerException $e) {
+                    // We may have upset Kitsu
+                    Log::info($e->getMessage());
+                    abort(500);
+                }
+            }
+        }
+    }
+}
