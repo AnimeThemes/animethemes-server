8f6ba607332880c406bfc4b1b513ca9dd99357bb
paranarimasu <33796518+paranarimasu@users.noreply.github.com>
2021-02-01 14:52:07 -0600

    fix(seeder): make anime index seeder rerunnable

--- database/seeders/AnimeSeeder.php
+++ database/seeders/AnimeSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Anime;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 
 class AnimeSeeder extends Seeder
@@ -16,26 +16,14 @@ class AnimeSeeder extends Seeder
      */
     public function run()
     {
-        // Remove any existing rows in Anime-related tables
-        // We want these tables to match the subreddit wiki
-        DB::table('entry_video')->delete();
-        DB::table('entry')->delete();
-        DB::table('theme')->delete();
-        DB::table('synonym')->delete();
-        DB::table('anime_resource')->delete();
-        DB::table('anime_series')->delete();
-        DB::table('anime')->delete();
-
         // Get JSON of Anime Index page content
-        $anime_wiki_contents = file_get_contents('https://old.reddit.com/r/AnimeThemes/wiki/anime_index.json');
+        $anime_wiki_contents = file_get_contents(WikiPages::ANIME_INDEX);
         $anime_wiki_json = json_decode($anime_wiki_contents);
         $anime_wiki_content_md = $anime_wiki_json->data->content_md;
 
         // Match Anime Entries
         // Format: "[{Anime Name} ({Year})](/r/AnimeThemes/wiki/{year}#{anchor link})"
         preg_match_all('/\[(.*)\s\((.*)\)\]\(\/r\/AnimeThemes\/wiki\/.*\)/m', $anime_wiki_content_md, $anime_wiki_entries, PREG_SET_ORDER);
-
-        $slugs = [];
 
         foreach ($anime_wiki_entries as $anime_wiki_entry) {
             $anime_name = html_entity_decode($anime_wiki_entry[1]);
@@ -44,23 +32,25 @@ class AnimeSeeder extends Seeder
             // Generate default slug
             // Fallback: append year if first attempt exists
             $slug = Str::slug($anime_name, '_');
-            if (in_array($slug, $slugs)) {
+            if (Anime::where('slug', $slug)->exists()) {
                 $slug = Str::slug($anime_name.' '.$anime_year, '_');
             }
-            $slugs[] = $slug;
 
             // Year expects a number but we group 60s, 70s, 80s & 90s
             // Fallback: Change group values to 1960, 1970, 1980 & 1990 for later inspection
-            if (strpos($anime_year, 's') !== false) {
-                $anime_year = '19'.str_replace('s', '', $anime_year);
-            }
+            $anime_years = WikiPages::getAnimeIndexYears($anime_year);
 
-            // Create Model from subreddit Name and Year and generated slug
-            Anime::create([
-                'name' => $anime_name,
-                'slug' => $slug,
-                'year' => $anime_year,
-            ]);
+            // Create Anime if it doesn't already exist
+            if (Anime::where('name', $anime_name)->whereIn('year', $anime_years)->doesntExist()) {
+                Log::info("Creating anime '{$anime_name}'");
+                Anime::create(
+                    [
+                        'name' => $anime_name,
+                        'slug' => $slug,
+                        'year' => $anime_years[0],
+                    ]
+                );
+            }
         }
     }
 }
