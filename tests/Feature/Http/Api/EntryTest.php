376b921a536e5bda571c9cf15251b04b53963493
paranarimasu <33796518+paranarimasu@users.noreply.github.com>
2021-02-22 13:47:30 -0600

    test: json api - resource endpoints

--- tests/Feature/Http/Api/EntryTest.php
+++ tests/Feature/Http/Api/EntryTest.php
@@ -2,112 +2,11 @@
 
 namespace Tests\Feature\Http\Api;
 
-use App\Models\Anime;
 use App\Models\Entry;
-use App\Models\Theme;
-use App\Models\Video;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class EntryTest extends TestCase
 {
-    use RefreshDatabase, WithFaker;
-
-    /**
-     * The Entry Index Endpoint shall display the Entry attributes.
-     *
-     * @return void
-     */
-    public function testEntryIndexAttributes()
-    {
-        $entries = Entry::factory()
-            ->for(Theme::factory()->for(Anime::factory()))
-            ->count($this->faker->randomDigitNotNull)
-            ->create();
-
-        $response = $this->get(route('api.entry.index'));
-
-        $response->assertJson([
-            'entries' => $entries->map(function ($entry) {
-                return static::getData($entry);
-            })->toArray(),
-        ]);
-    }
-
-    /**
-     * The Show Entry Endpoint shall display the Entry attributes.
-     *
-     * @return void
-     */
-    public function testShowEntryAttributes()
-    {
-        $entry = Entry::factory()
-            ->for(Theme::factory()->for(Anime::factory()))
-            ->create();
-
-        $response = $this->get(route('api.entry.show', ['entry' => $entry]));
-
-        $response->assertJson(static::getData($entry));
-    }
-
-    /**
-     * The Show Entry Endpoint shall display the anime relation in an 'anime' attribute.
-     *
-     * @return void
-     */
-    public function testShowEntryAnimeAttributes()
-    {
-        $entry = Entry::factory()
-            ->for(Theme::factory()->for(Anime::factory()))
-            ->create();
-
-        $response = $this->get(route('api.entry.show', ['entry' => $entry]));
-
-        $response->assertJson([
-            'anime' => AnimeTest::getData($entry->anime),
-        ]);
-    }
-
-    /**
-     * The Show Entry Endpoint shall display the theme relation in a 'theme' attribute.
-     *
-     * @return void
-     */
-    public function testShowEntryThemeAttributes()
-    {
-        $entry = Entry::factory()
-            ->for(Theme::factory()->for(Anime::factory()))
-            ->create();
-
-        $response = $this->get(route('api.entry.show', ['entry' => $entry]));
-
-        $response->assertJson([
-            'theme' => ThemeTest::getData($entry->theme),
-        ]);
-    }
-
-    /**
-     * The Show Entry Endpoint shall display the videos relation in a 'videos' attribute.
-     *
-     * @return void
-     */
-    public function testShowEntryVideosAttribute()
-    {
-        $entry = Entry::factory()
-            ->for(Theme::factory()->for(Anime::factory()))
-            ->has(Video::factory()->count($this->faker->randomDigitNotNull))
-            ->create();
-
-        $response = $this->get(route('api.entry.show', ['entry' => $entry]));
-
-        $response->assertJson([
-            'videos' => $entry->videos->map(function ($video) {
-                return VideoTest::getData($video);
-            })->toArray(),
-        ]);
-    }
-
     /**
      * Get attributes for Entry resource.
      *
---
+++ tests/Feature/Http/Api/ExternalResource/ExternalResourceShowTest.php
@@ -0,0 +1,214 @@
+<?php
+
+namespace Tests\Feature\Http\Api\ExternalResource;
+
+use App\Enums\AnimeSeason;
+use App\Http\Resources\ExternalResourceResource;
+use App\JsonApi\QueryParser;
+use App\Models\Anime;
+use App\Models\Artist;
+use App\Models\ExternalResource;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ExternalResourceShowTest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+
+    /**
+     * By default, the Resource Show Endpoint shall return an ExternalResource Resource with all allowed include paths.
+     *
+     * @return void
+     */
+    public function testDefault()
+    {
+        ExternalResource::factory()
+            ->has(Anime::factory()->count($this->faker->randomDigitNotNull))
+            ->has(Artist::factory()->count($this->faker->randomDigitNotNull))
+            ->create();
+
+        $resource = ExternalResource::with(ExternalResourceResource::allowedIncludePaths())->first();
+
+        $response = $this->get(route('api.resource.show', ['resource' => $resource]));
+
+        $response->assertJson(
+            json_decode(
+                json_encode(
+                    ExternalResourceResource::make($resource, QueryParser::make())
+                        ->response()
+                        ->getData()
+                ),
+                true
+            )
+        );
+    }
+
+    /**
+     * The Resource Show Endpoint shall allow inclusion of related resources.
+     *
+     * @return void
+     */
+    public function testAllowedIncludePaths()
+    {
+        $allowed_paths = collect(ExternalResourceResource::allowedIncludePaths());
+        $included_paths = $allowed_paths->random($this->faker->numberBetween(0, count($allowed_paths)));
+
+        $parameters = [
+            QueryParser::PARAM_INCLUDE => $included_paths->join(','),
+        ];
+
+        ExternalResource::factory()
+            ->has(Anime::factory()->count($this->faker->randomDigitNotNull))
+            ->has(Artist::factory()->count($this->faker->randomDigitNotNull))
+            ->create();
+
+        $resource = ExternalResource::with($included_paths->all())->first();
+
+        $response = $this->get(route('api.resource.show', ['resource' => $resource] + $parameters));
+
+        $response->assertJson(
+            json_decode(
+                json_encode(
+                    ExternalResourceResource::make($resource, QueryParser::make($parameters))
+                        ->response()
+                        ->getData()
+                ),
+                true
+            )
+        );
+    }
+
+    /**
+     * The Resource Show Endpoint shall implement sparse fieldsets.
+     *
+     * @return void
+     */
+    public function testSparseFieldsets()
+    {
+        $fields = collect([
+            'id',
+            'link',
+            'external_id',
+            'site',
+            'as',
+            'created_at',
+            'updated_at',
+        ]);
+
+        $included_fields = $fields->random($this->faker->numberBetween(0, count($fields)));
+
+        $parameters = [
+            QueryParser::PARAM_FIELDS => [
+                ExternalResourceResource::$resourceType => $included_fields->join(','),
+            ],
+        ];
+
+        ExternalResource::factory()
+            ->has(Anime::factory()->count($this->faker->randomDigitNotNull))
+            ->has(Artist::factory()->count($this->faker->randomDigitNotNull))
+            ->create();
+
+        $resource = ExternalResource::with(ExternalResourceResource::allowedIncludePaths())->first();
+
+        $response = $this->get(route('api.resource.show', ['resource' => $resource] + $parameters));
+
+        $response->assertJson(
+            json_decode(
+                json_encode(
+                    ExternalResourceResource::make($resource, QueryParser::make($parameters))
+                        ->response()
+                        ->getData()
+                ),
+                true
+            )
+        );
+    }
+
+    /**
+     * The Resource Show Endpoint shall support constrained eager loading of anime by season.
+     *
+     * @return void
+     */
+    public function testAnimeBySeason()
+    {
+        $season_filter = AnimeSeason::getRandomInstance();
+
+        $parameters = [
+            QueryParser::PARAM_FILTER => [
+                'season' => $season_filter->key,
+            ],
+        ];
+
+        ExternalResource::factory()
+            ->has(Anime::factory()->count($this->faker->randomDigitNotNull))
+            ->create();
+
+        $resource = ExternalResource::with([
+            'anime' => function ($query) use ($season_filter) {
+                $query->where('season', $season_filter->value);
+            },
+        ])
+        ->first();
+
+        $response = $this->get(route('api.resource.show', ['resource' => $resource] + $parameters));
+
+        $response->assertJson(
+            json_decode(
+                json_encode(
+                    ExternalResourceResource::make($resource, QueryParser::make($parameters))
+                        ->response()
+                        ->getData()
+                ),
+                true
+            )
+        );
+    }
+
+    /**
+     * The Resource Show Endpoint shall support constrained eager loading of anime by year.
+     *
+     * @return void
+     */
+    public function testAnimeByYear()
+    {
+        $year_filter = $this->faker->year();
+        $excluded_year = $year_filter + 1;
+
+        $parameters = [
+            QueryParser::PARAM_FILTER => [
+                'year' => $year_filter,
+            ],
+        ];
+
+        ExternalResource::factory()
+            ->has(
+                Anime::factory()
+                ->count($this->faker->randomDigitNotNull)
+                ->state([
+                    'year' => $this->faker->boolean() ? $year_filter : $excluded_year,
+                ])
+            )
+            ->create();
+
+        $resource = ExternalResource::with([
+            'anime' => function ($query) use ($year_filter) {
+                $query->where('year', $year_filter);
+            },
+        ])
+        ->first();
+
+        $response = $this->get(route('api.resource.show', ['resource' => $resource] + $parameters));
+
+        $response->assertJson(
+            json_decode(
+                json_encode(
+                    ExternalResourceResource::make($resource, QueryParser::make($parameters))
+                        ->response()
+                        ->getData()
+                ),
+                true
+            )
+        );
+    }
+}
