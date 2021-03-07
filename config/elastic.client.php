8d3fb29b7c27dc584914595914dc088eedebb3bf
paranarimasu <33796518+paranarimasu@users.noreply.github.com>
2021-03-07 17:51:03 -0600

    fix(api): allow elastic host configuration (#133)

--- config/elastic.client.php
+++ config/elastic.client.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 return [
     'hosts' => [
-        env('ELASTIC_HOST', 'localhost:9200'),
+        [
+            'host' => env('ELASTIC_HOST'),
+            'port' => env('ELASTIC_PORT'),
+            'scheme' => env('ELASTIC_SCHEME'),
+            'user' => env('ELASTIC_USER'),
+            'pass' => env('ELASTIC_PASS'),
+        ],
     ],
 ];
