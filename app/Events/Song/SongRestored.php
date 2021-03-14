8516b4ad884474828f75c06b9d9cf87097b84518
paranarimasu <33796518+paranarimasu@users.noreply.github.com>
2021-03-14 01:02:10 -0600

    style: fix StyleCI findings

--- app/Events/Song/SongRestored.php
+++ app/Events/Song/SongRestored.php
@@ -4,11 +4,11 @@ namespace App\Events\Song;
 
 use App\Contracts\Events\DiscordMessageEvent;
 use App\Contracts\Events\UpdateRelatedIndicesEvent;
-use Illuminate\Foundation\Events\Dispatchable;
 use App\Models\Artist;
 use App\Models\Entry;
 use App\Models\Theme;
 use App\Models\Video;
+use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Support\Facades\Config;
 use NotificationChannels\Discord\DiscordMessage;
 
