4097dfd27f28367b39aaf4a78b11fd9aa175b61f
paranarimasu <33796518+paranarimasu@users.noreply.github.com>
2021-03-15 23:17:33 -0500

    fix(config): addressing enlightn findings, updating framework stubs

--- config/jetstream.php
+++ config/jetstream.php
@@ -18,6 +18,19 @@ return [
     'stack' => 'livewire',
 
     /*
+     |--------------------------------------------------------------------------
+     | Jetstream Route Middleware
+     |--------------------------------------------------------------------------
+     |
+     | Here you may specify which middleware Jetstream will assign to the routes
+     | that it registers with the application. When necessary, you may modify
+     | these middleware; however, this default value is usually sufficient.
+     |
+     */
+
+    'middleware' => ['web'],
+
+    /*
     |--------------------------------------------------------------------------
     | Features
     |--------------------------------------------------------------------------
@@ -29,9 +42,23 @@ return [
     */
 
     'features' => [
+        // Features::termsAndPrivacyPolicy(),
         // Features::profilePhotos(),
         // Features::api(),
-        // Features::teams(),
+        // Features::teams(['invitations' => true]),
+        Features::accountDeletion(),
     ],
 
+    /*
+    |--------------------------------------------------------------------------
+    | Profile Photo Disk
+    |--------------------------------------------------------------------------
+    |
+    | This configuration value determines the default disk that will be used
+    | when storing profile photos for your application's users. Typically
+    | this will be the "public" disk but you may adjust this if needed.
+    |
+    */
+
+    'profile_photo_disk' => 'public',
 ];
