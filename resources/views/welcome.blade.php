ba0e8b5cc066203ee524ebbc6009484d4b6d8cdb
paranarimasu <33796518+paranarimasu@users.noreply.github.com>
2021-03-20 20:33:39 -0500

    fix(ux): add announcement margin (#148)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,7 +15,7 @@
     <!-- Announcements -->
     <div class="container sm:max-w-4xl mx-auto px-6">
         @foreach ($announcements as $announcement)
-            <div class="px-4 py-3 leading-normal text-blue-700 bg-blue-100 rounded-lg">
+            <div class="px-4 py-3 m-6 leading-normal text-blue-700 bg-blue-100 rounded-lg">
                 <svg class="inline align-middle" xmlns="http://www.w3.org/2000/svg" width="20" viewBox="0 0 20 20" fill="currentColor">
                     <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd" />
                 </svg>
