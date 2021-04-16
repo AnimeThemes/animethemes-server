8d19c87fb1bc68d116d4b080330f06788995901b
paranarimasu <33796518+paranarimasu@users.noreply.github.com>
2021-04-16 17:04:40 -0500

    fix(discord): do not raise events on restore save operation (#182)

--- app/Models/User.php
+++ app/Models/User.php
@@ -83,6 +83,39 @@ class User extends Authenticatable implements MustVerifyEmail, Nameable
         return $this->name;
     }
 
+    /**
+     * Restore a soft-deleted model instance.
+     *
+     * @return bool|null
+     */
+    public function restore()
+    {
+        // If the restoring event does not return false, we will proceed with this
+        // restore operation. Otherwise, we bail out so the developer will stop
+        // the restore totally. We will clear the deleted timestamp and save.
+        if ($this->fireModelEvent('restoring') === false) {
+            return false;
+        }
+
+        $this->{$this->getDeletedAtColumn()} = null;
+
+        // Once we have saved the model, we will fire the "restored" event so this
+        // developer will do anything they need to after a restore operation is
+        // totally finished. Then we will return the result of the save call.
+        $this->exists = true;
+
+        // Save quietly so that we do not fire an updated event on restore
+        $result = self::withoutEvents(
+            function () {
+                return $this->save();
+            }
+        );
+
+        $this->fireModelEvent('restored', false);
+
+        return $result;
+    }
+
     // Make HasTeams more null safe
 
     /**
