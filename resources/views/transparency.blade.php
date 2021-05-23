b118bacb73fa2cfacece65acb6f7ac9e4fa17d6e
paranarimasu <33796518+paranarimasu@users.noreply.github.com>
2021-05-23 14:16:25 -0500

    fix(transparency): adding subheadings and better styling for readability

--- resources/views/transparency.blade.php
+++ resources/views/transparency.blade.php
@@ -3,6 +3,7 @@
         <div class="py-8">
             <div>
                 <h2 class="text-2xl font-semibold leading-tight">{{ __('Balances') }}</h2>
+                <p class="mt-1 text-sm text-gray-600">{{ __('A listing of account balances against current monthly usage. All amounts are expressed in USD.') }}</p>
             </div>
 
             <div class="flex flex-col">
@@ -35,17 +36,17 @@
                                     </tr>
                                     @endif
                                     @foreach ($balances as $balance)
-                                    <tr>
-                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                    <tr class="whitespace-nowrap text-sm text-gray-500">
+                                        <td class="px-6 py-4">
                                             {{ $balance->service->description }}
                                         </td>
-                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                        <td class="px-6 py-4">
                                             {{ $balance->frequency->description }}
                                         </td>
-                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                        <td class="px-6 py-4 font-bold {{ $balance->usage > 0 ? 'text-red-500' : '' }} {{ $balance->usage < 0 ? 'text-green-500' : '' }}">
                                             {{ $balance->usage }}
                                         </td>
-                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                        <td class="px-6 py-4 font-bold {{ $balance->balance < 0 ? 'text-red-500' : '' }} {{ $balance->balance > 0 ? 'text-green-500' : '' }}">
                                             {{ $balance->balance }}
                                         </td>
                                     </tr>
@@ -61,6 +62,7 @@
         <div class="py-8">
             <div>
                 <h2 class="text-2xl font-semibold leading-tight">{{ __('Transactions') }}</h2>
+                <p class="mt-1 text-sm text-gray-600">{{ __('A listing of invoices and payments made this month. All amounts are expressed in USD.') }}</p>
             </div>
 
             <div class="flex flex-col">
@@ -93,17 +95,17 @@
                                     </tr>
                                     @endif
                                     @foreach ($transactions as $transaction)
-                                    <tr>
-                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                    <tr class="whitespace-nowrap text-sm text-gray-500">
+                                        <td class="px-6 py-4">
                                             {{ $transaction->date->format('Y-m-d') }}
                                         </td>
-                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                        <td class="px-6 py-4">
                                             {{ $transaction->service->description }}
                                         </td>
-                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                        <td class="px-6 py-4 font-bold {{ $balance->usage >= 0 ? 'text-red-500' : 'text-green-500' }}">
                                             {{ $transaction->amount }}
                                         </td>
-                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                        <td class="px-6 py-4">
                                             {{ $transaction->description }}
                                         </td>
                                     </tr>
