a636a673cce0e3dc77aa8f77fc479101e79f1f68
paranarimasu <33796518+paranarimasu@users.noreply.github.com>
2021-07-21 12:28:39 -0500

    #203 fix(auth): exclude sensitive data from parameter logging

--- app/Http/Middleware/LogRequest.php
+++ app/Http/Middleware/LogRequest.php
@@ -14,21 +14,6 @@ use Illuminate\Support\Facades\Log;
 class LogRequest
 {
     /**
-     * The list of parameters to exclude from logging.
-     *
-     * @var array
-     */
-    protected array $hidden = [
-        'password',
-        'current_password',
-        'password_confirmation',
-        'remember_token',
-        'two_factor_recovery_codes',
-        'two_factor_secret',
-        '_token',
-    ];
-
-    /**
      * Handle an incoming request.
      *
      * @param Request $request
@@ -41,7 +26,6 @@ class LogRequest
             'method' => $request->method(),
             'full-url' => $request->fullUrl(),
             'ip' => $request->ip(),
-            'parameters' => $request->except($this->hidden),
             'headers' => $request->headers->all(),
         ]);
 
