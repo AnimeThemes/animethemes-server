23d15dbd7ef501fd30c19c6ad591a261d46cd49c
ProWeebDev <34277874+ProWeebDev@users.noreply.github.com>
2021-11-04 02:39:57 -0500

    Created migration for the studio resource pivot

---
+++ database/migrations/2021_11_03_003056_create_studio_resource.php
@@ -0,0 +1,40 @@
+<?php
+
+
+use App\Models\Wiki\Studio;
+use App\Models\Wiki\ExternalResource;
+use App\Pivots\StudioResource;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudioResource extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create(StudioResource::TABLE, function (Blueprint $table) {
+            $table->timestamps(6);
+            $table->unsignedBigInteger(StudioResource::ATTRIBUTE_STUDIO);
+            $table->foreign(StudioResource::ATTRIBUTE_STUDIO)->references(Studio::ATTRIBUTE_ID)->on(Studio::TABLE)->cascadeOnDelete();
+            $table->unsignedBigInteger(StudioResource::ATTRIBUTE_RESOURCE);
+            $table->foreign(StudioResource::ATTRIBUTE_RESOURCE)->references(ExternalResource::ATTRIBUTE_ID)->on(ExternalResource::TABLE)->cascadeOnDelete();
+            $table->primary([StudioResource::ATTRIBUTE_STUDIO, StudioResource::ATTRIBUTE_RESOURCE]);
+            $table->string(StudioResource::ATTRIBUTE_AS)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists(StudioResource::TABLE);
+    }
+}
