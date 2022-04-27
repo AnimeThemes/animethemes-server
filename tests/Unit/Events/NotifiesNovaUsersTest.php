79c44dae77661b4fe2b143389bd5ce538176e4c2
paranarimasu <33796518+paranarimasu@users.noreply.github.com>
2022-04-27 15:45:42 -0500

    style: fix StyleCI findings

--- tests/Unit/Events/NotifiesNovaUsersTest.php
+++ tests/Unit/Events/NotifiesNovaUsersTest.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\Unit\Events;
 
-use App\Contracts\Events\CascadesDeletesEvent;
 use App\Contracts\Events\NovaNotificationEvent;
-use App\Listeners\CascadesDeletes;
 use App\Listeners\NotifiesNovaUsers;
 use Illuminate\Support\Facades\Event;
 use Tests\TestCase;
