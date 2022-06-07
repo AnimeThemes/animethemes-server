0e57426d1a2a4b8b8f81170276d691ec35b0aac5
paranarimasu <33796518+paranarimasu@users.noreply.github.com>
2022-06-07 00:54:38 -0500

    style: fix StyleCI finding

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -50,7 +50,7 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->routes(function () {
             Route::middleware('web')
-		        ->domain(Config::get('web.url'))
+                ->domain(Config::get('web.url'))
                 ->prefix(Config::get('web.path'))
                 ->group(base_path('routes/web.php'));
 
