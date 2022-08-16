9a0989a3f7b84d4c62e43b2c9a87a6e1a8b929ea
paranarimasu <33796518+paranarimasu@users.noreply.github.com>
2022-08-16 16:59:51 -0500

    fix(api): update elastic client config (#440)

--- config/elastic.client.php
+++ config/elastic.client.php
@@ -3,13 +3,18 @@
 declare(strict_types=1);
 
 return [
-    'hosts' => [
-        [
-            'host' => env('ELASTIC_HOST', 'localhost'),
-            'port' => env('ELASTIC_PORT', '9200'),
-            'scheme' => env('ELASTIC_SCHEME', 'http'),
-            'user' => env('ELASTIC_USER'),
-            'pass' => env('ELASTIC_PASS'),
+    'default' => env('ELASTIC_CONNECTION', 'default'),
+    'connections' => [
+        'default' => [
+            'hosts' => [
+                [
+                    'host' => env('ELASTIC_HOST', 'localhost'),
+                    'port' => env('ELASTIC_PORT', '9200'),
+                    'scheme' => env('ELASTIC_SCHEME', 'http'),
+                    'user' => env('ELASTIC_USER'),
+                    'pass' => env('ELASTIC_PASS'),
+                ],
+            ],
         ],
     ],
 ];
