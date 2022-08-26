bc9f69038ad917b85dad616f5db1b70a3b3d7871
paranarimasu <33796518+paranarimasu@users.noreply.github.com>
2022-08-26 12:44:42 -0500

    refactor: use model constants in migration

--- database/migrations/2017_08_24_000000_create_settings_table.php
+++ database/migrations/2017_08_24_000000_create_settings_table.php
@@ -2,9 +2,9 @@
 
 declare(strict_types=1);
 
+use App\Models\Admin\Setting;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -16,11 +16,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        if (! Schema::hasTable(Config::get('setting.database.table'))) {
-            Schema::create(Config::get('setting.database.table'), function (Blueprint $table) {
-                $table->increments('id');
-                $table->string(Config::get('setting.database.key'))->index();
-                $table->text(Config::get('setting.database.value'));
+        if (! Schema::hasTable(Setting::TABLE)) {
+            Schema::create(Setting::TABLE, function (Blueprint $table) {
+                $table->id();
+                $table->string(Setting::ATTRIBUTE_KEY)->index();
+                $table->text(Setting::ATTRIBUTE_VALUE);
             });
         }
     }
@@ -32,6 +32,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists(Config::get('setting.database.table'));
+        Schema::dropIfExists(Setting::TABLE);
     }
 };
