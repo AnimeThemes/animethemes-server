1275f9e396c7dc2d0b39e2a28e985d53b12a3c3a
paranarimasu <33796518+paranarimasu@users.noreply.github.com>
2023-02-17 22:23:57 -0600

    feat(api): adding crud endpoints for anime resource pivot type

---
+++ app/Http/Api/Field/Pivot/Wiki/AnimeResource/AnimeResourceAnimeIdField.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Api\Field\Pivot\Wiki\AnimeResource;
+
+use App\Contracts\Http\Api\Field\CreatableField;
+use App\Contracts\Http\Api\Field\SelectableField;
+use App\Http\Api\Field\Field;
+use App\Http\Api\Query\Query;
+use App\Http\Api\Schema\Schema;
+use App\Models\Wiki\Anime;
+use App\Pivots\Wiki\AnimeResource;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+/**
+ * Class AnimeResourceAnimeIdField.
+ */
+class AnimeResourceAnimeIdField extends Field implements CreatableField, SelectableField
+{
+    /**
+     * Create a new field instance.
+     *
+     * @param  Schema  $schema
+     */
+    public function __construct(Schema $schema)
+    {
+        parent::__construct($schema, AnimeResource::ATTRIBUTE_ANIME);
+    }
+
+    /**
+     * Set the creation validation rules for the field.
+     *
+     * @param  Request  $request
+     * @return array
+     */
+    public function getCreationRules(Request $request): array
+    {
+        return [
+            'required',
+            'integer',
+            Rule::exists(Anime::TABLE, Anime::ATTRIBUTE_ID),
+        ];
+    }
+
+    /**
+     * Determine if the field should be included in the select clause of our query.
+     *
+     * @param  Query  $query
+     * @param  Schema  $schema
+     * @return bool
+     */
+    public function shouldSelect(Query $query, Schema $schema): bool
+    {
+        // Needed to match anime relation.
+        return true;
+    }
+}
---
+++ tests/Feature/Http/Api/Pivot/Wiki/AnimeResource/AnimeResourceDestroyTest.php
@@ -0,0 +1,100 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Http\Api\Pivot\Wiki\AnimeResource;
+
+use App\Models\Auth\User;
+use App\Models\Wiki\Anime;
+use App\Models\Wiki\ExternalResource;
+use App\Pivots\Wiki\AnimeResource;
+use Illuminate\Foundation\Testing\WithoutEvents;
+use Laravel\Sanctum\Sanctum;
+use Tests\TestCase;
+
+/**
+ * Class AnimeResourceDestroyTest.
+ */
+class AnimeResourceDestroyTest extends TestCase
+{
+    use WithoutEvents;
+
+    /**
+     * The Anime Resource Destroy Endpoint shall be protected by sanctum.
+     *
+     * @return void
+     */
+    public function testProtected(): void
+    {
+        $animeResource = AnimeResource::factory()
+            ->for(Anime::factory())
+            ->for(ExternalResource::factory(), AnimeResource::RELATION_RESOURCE)
+            ->createOne();
+
+        $response = $this->delete(route('api.animeresource.destroy', ['anime' => $animeResource->anime, 'resource' => $animeResource->resource]));
+
+        $response->assertUnauthorized();
+    }
+
+    /**
+     * The Anime Resource Destroy Endpoint shall forbid users without the delete anime & delete resource permissions.
+     *
+     * @return void
+     */
+    public function testForbidden(): void
+    {
+        $animeResource = AnimeResource::factory()
+            ->for(Anime::factory())
+            ->for(ExternalResource::factory(), AnimeResource::RELATION_RESOURCE)
+            ->createOne();
+
+        $user = User::factory()->createOne();
+
+        Sanctum::actingAs($user);
+
+        $response = $this->delete(route('api.animeresource.destroy', ['anime' => $animeResource->anime, 'resource' => $animeResource->resource]));
+
+        $response->assertForbidden();
+    }
+
+    /**
+     * The Anime Resource Destroy Endpoint shall return an error if the anime resource does not exist.
+     *
+     * @return void
+     */
+    public function testNotFound(): void
+    {
+        $anime = Anime::factory()->createOne();
+        $resource = ExternalResource::factory()->createOne();
+
+        $user = User::factory()->withPermissions(['delete anime', 'delete external resource'])->createOne();
+
+        Sanctum::actingAs($user);
+
+        $response = $this->delete(route('api.animeresource.destroy', ['anime' => $anime, 'resource' => $resource]));
+
+        $response->assertNotFound();
+    }
+
+    /**
+     * The Anime Resource Destroy Endpoint shall delete the anime resource.
+     *
+     * @return void
+     */
+    public function testDeleted(): void
+    {
+        $animeResource = AnimeResource::factory()
+            ->for(Anime::factory())
+            ->for(ExternalResource::factory(), AnimeResource::RELATION_RESOURCE)
+            ->createOne();
+
+        $user = User::factory()->withPermissions(['delete anime', 'delete external resource'])->createOne();
+
+        Sanctum::actingAs($user);
+
+        $response = $this->delete(route('api.animeresource.destroy', ['anime' => $animeResource->anime, 'resource' => $animeResource->resource]));
+
+        $response->assertOk();
+        static::assertModelMissing($animeResource);
+    }
+}
