808d0cd56d5cb2ca07f934db315ef15c92fed781
paranarimasu <33796518+paranarimasu@users.noreply.github.com>
2023-02-23 00:53:54 -0600

    feat(api): adding anime theme entry video pivot endpoints (#524)

---
+++ tests/Feature/Http/Api/Pivot/Wiki/AnimeThemeEntryVideo/AnimeThemeEntryVideoDestroyTest.php
@@ -0,0 +1,105 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Http\Api\Pivot\Wiki\AnimeThemeEntryVideo;
+
+use App\Models\Auth\User;
+use App\Models\Wiki\Anime;
+use App\Models\Wiki\Anime\AnimeTheme;
+use App\Models\Wiki\Anime\Theme\AnimeThemeEntry;
+use App\Models\Wiki\Video;
+use App\Pivots\Wiki\AnimeThemeEntryVideo;
+use Illuminate\Foundation\Testing\WithoutEvents;
+use Laravel\Sanctum\Sanctum;
+use Tests\TestCase;
+
+/**
+ * Class AnimeThemeEntryVideoDestroyTest.
+ */
+class AnimeThemeEntryVideoDestroyTest extends TestCase
+{
+    use WithoutEvents;
+
+    /**
+     * The Anime Theme Entry Video Destroy Endpoint shall be protected by sanctum.
+     *
+     * @return void
+     */
+    public function testProtected(): void
+    {
+        $entryVideo = AnimeThemeEntryVideo::factory()
+            ->for(AnimeThemeEntry::factory()->for(AnimeTheme::factory()->for(Anime::factory())))
+            ->for(Video::factory())
+            ->createOne();
+
+        $response = $this->delete(route('api.animethemeentryvideo.destroy', ['animethemeentry' => $entryVideo->animethemeentry, 'video' => $entryVideo->video]));
+
+        $response->assertUnauthorized();
+    }
+
+    /**
+     * The Anime Theme Entry Video Destroy Endpoint shall forbid users without the delete anime theme entry & delete video permissions.
+     *
+     * @return void
+     */
+    public function testForbidden(): void
+    {
+        $entryVideo = AnimeThemeEntryVideo::factory()
+            ->for(AnimeThemeEntry::factory()->for(AnimeTheme::factory()->for(Anime::factory())))
+            ->for(Video::factory())
+            ->createOne();
+
+        $user = User::factory()->createOne();
+
+        Sanctum::actingAs($user);
+
+        $response = $this->delete(route('api.animethemeentryvideo.destroy', ['animethemeentry' => $entryVideo->animethemeentry, 'video' => $entryVideo->video]));
+
+        $response->assertForbidden();
+    }
+
+    /**
+     * The Anime Theme Entry Video Destroy Endpoint shall return an error if the anime theme entry video does not exist.
+     *
+     * @return void
+     */
+    public function testNotFound(): void
+    {
+        $entry = AnimeThemeEntry::factory()
+            ->for(AnimeTheme::factory()->for(Anime::factory()))
+            ->create();
+
+        $video = Video::factory()->createOne();
+
+        $user = User::factory()->withPermissions(['delete anime theme entry', 'delete video'])->createOne();
+
+        Sanctum::actingAs($user);
+
+        $response = $this->delete(route('api.animethemeentryvideo.destroy', ['animethemeentry' => $entry, 'video' => $video]));
+
+        $response->assertNotFound();
+    }
+
+    /**
+     * The Anime Theme Entry Video Destroy Endpoint shall delete the anime theme entry video.
+     *
+     * @return void
+     */
+    public function testDeleted(): void
+    {
+        $entryVideo = AnimeThemeEntryVideo::factory()
+            ->for(AnimeThemeEntry::factory()->for(AnimeTheme::factory()->for(Anime::factory())))
+            ->for(Video::factory())
+            ->createOne();
+
+        $user = User::factory()->withPermissions(['delete anime theme entry', 'delete video'])->createOne();
+
+        Sanctum::actingAs($user);
+
+        $response = $this->delete(route('api.animethemeentryvideo.destroy', ['animethemeentry' => $entryVideo->animethemeentry, 'video' => $entryVideo->video]));
+
+        $response->assertOk();
+        static::assertModelMissing($entryVideo);
+    }
+}
---
+++ tests/Feature/Http/Api/Pivot/Wiki/AnimeThemeEntryVideo/AnimeThemeEntryVideoStoreTest.php
@@ -0,0 +1,106 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Http\Api\Pivot\Wiki\AnimeThemeEntryVideo;
+
+use App\Models\Auth\User;
+use App\Models\Wiki\Anime;
+use App\Models\Wiki\Anime\AnimeTheme;
+use App\Models\Wiki\Anime\Theme\AnimeThemeEntry;
+use App\Models\Wiki\Video;
+use App\Pivots\Wiki\AnimeThemeEntryVideo;
+use Illuminate\Foundation\Testing\WithoutEvents;
+use Laravel\Sanctum\Sanctum;
+use Tests\TestCase;
+
+/**
+ * Class AnimeThemeEntryVideoStoreTest.
+ */
+class AnimeThemeEntryVideoStoreTest extends TestCase
+{
+    use WithoutEvents;
+
+    /**
+     * The Anime Theme Entry Video Store Destroy Endpoint shall be protected by sanctum.
+     *
+     * @return void
+     */
+    public function testProtected(): void
+    {
+        $entryVideo = AnimeThemeEntryVideo::factory()
+            ->for(AnimeThemeEntry::factory()->for(AnimeTheme::factory()->for(Anime::factory())))
+            ->for(Video::factory())
+            ->makeOne();
+
+        $response = $this->post(route('api.animethemeentryvideo.store', $entryVideo->toArray()));
+
+        $response->assertUnauthorized();
+    }
+
+    /**
+     * The Anime Theme Entry Video Store Endpoint shall forbid users without the create anime theme entry & create video permissions.
+     *
+     * @return void
+     */
+    public function testForbidden(): void
+    {
+        $entryVideo = AnimeThemeEntryVideo::factory()
+            ->for(AnimeThemeEntry::factory()->for(AnimeTheme::factory()->for(Anime::factory())))
+            ->for(Video::factory())
+            ->makeOne();
+
+        $user = User::factory()->createOne();
+
+        Sanctum::actingAs($user);
+
+        $response = $this->post(route('api.animethemeentryvideo.store', $entryVideo->toArray()));
+
+        $response->assertForbidden();
+    }
+
+    /**
+     * The Anime Theme Entry Video Store Endpoint shall require anime theme entry and video fields.
+     *
+     * @return void
+     */
+    public function testRequiredFields(): void
+    {
+        $user = User::factory()->withPermissions(['create anime theme entry', 'create video'])->createOne();
+
+        Sanctum::actingAs($user);
+
+        $response = $this->post(route('api.animethemeentryvideo.store'));
+
+        $response->assertJsonValidationErrors([
+            AnimeThemeEntryVideo::ATTRIBUTE_ENTRY,
+            AnimeThemeEntryVideo::ATTRIBUTE_VIDEO,
+        ]);
+    }
+
+    /**
+     * The Anime Theme Entry Video Store Endpoint shall create an Anime Theme Entry Video.
+     *
+     * @return void
+     */
+    public function testCreate(): void
+    {
+        $entry = AnimeThemeEntry::factory()
+            ->for(AnimeTheme::factory()->for(Anime::factory()))
+            ->createOne();
+
+        $parameters = [
+            AnimeThemeEntryVideo::ATTRIBUTE_ENTRY => $entry->getKey(),
+            AnimeThemeEntryVideo::ATTRIBUTE_VIDEO => Video::factory()->createOne()->getKey(),
+        ];
+
+        $user = User::factory()->withPermissions(['create anime theme entry', 'create video'])->createOne();
+
+        Sanctum::actingAs($user);
+
+        $response = $this->post(route('api.animethemeentryvideo.store', $parameters));
+
+        $response->assertCreated();
+        static::assertDatabaseCount(AnimeThemeEntryVideo::TABLE, 1);
+    }
+}
