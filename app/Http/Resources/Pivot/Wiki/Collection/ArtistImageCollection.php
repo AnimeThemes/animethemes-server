47074aee36dd138dddc6f25d9691e6361c88ff24
paranarimasu <33796518+paranarimasu@users.noreply.github.com>
2023-02-24 00:53:16 -0600

    feat(api): adding artist image pivot endpoints (#526)

---
+++ app/Http/Resources/Pivot/Wiki/Collection/ArtistImageCollection.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Resources\Pivot\Wiki\Collection;
+
+use App\Http\Resources\BaseCollection;
+use App\Http\Resources\Pivot\Wiki\Resource\ArtistImageResource;
+use App\Pivots\Wiki\ArtistImage;
+use Illuminate\Http\Request;
+
+/**
+ * Class ArtistImageCollection.
+ */
+class ArtistImageCollection extends BaseCollection
+{
+    /**
+     * The "data" wrapper that should be applied.
+     *
+     * @var string|null
+     */
+    public static $wrap = 'artistimages';
+
+    /**
+     * Transform the resource into a JSON array.
+     *
+     * @param  Request  $request
+     * @return array
+     *
+     * @noinspection PhpMissingParentCallCommonInspection
+     */
+    public function toArray($request): array
+    {
+        return $this->collection->map(fn (ArtistImage $artistImage) => new ArtistImageResource($artistImage, $this->query))->all();
+    }
+}
