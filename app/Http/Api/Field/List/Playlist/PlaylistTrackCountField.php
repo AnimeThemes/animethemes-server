37f6ab1bc158ba02aa40b2fee22a724bac8db8e5
paranarimasu <33796518+paranarimasu@users.noreply.github.com>
2023-03-22 13:42:09 -0500

    feat(api): adding playlist tracks_count aggregate field (#562)

---
+++ app/Http/Api/Field/List/Playlist/PlaylistTrackCountField.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Api\Field\List\Playlist;
+
+use App\Http\Api\Field\Aggregate\CountField;
+use App\Http\Api\Schema\Schema;
+use App\Models\List\Playlist;
+
+/**
+ * Class PlaylistTrackCountField.
+ */
+class PlaylistTrackCountField extends CountField
+{
+    /**
+     * Create a new field instance.
+     *
+     * @param  Schema  $schema
+     */
+    public function __construct(Schema $schema)
+    {
+        parent::__construct($schema, Playlist::RELATION_TRACKS);
+    }
+}
