5f5d015b798db90e638a018a4c8511eb3055cdad
Kyrch <kyrchk5@gmail.com>
2023-09-17 19:24:50 -0300

    feat: added more lenses (#590)

---
+++ app/Nova/Lenses/Anime/Resource/AnimeOfficialSiteResourceLens.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Nova\Lenses\Anime\Resource;
+
+use App\Enums\Models\Wiki\ResourceSite;
+use App\Nova\Lenses\Anime\AnimeResourceLens;
+
+/**
+ * Class AnimeOfficialSiteResourceLens.
+ */
+class AnimeOfficialSiteResourceLens extends AnimeResourceLens
+{
+    /**
+     * The resource site.
+     *
+     * @return ResourceSite
+     */
+    protected static function site(): ResourceSite
+    {
+        return ResourceSite::OFFICIAL_SITE;
+    }
+
+    /**
+     * Get the URI key for the lens.
+     *
+     * @return string
+     *
+     * @noinspection PhpMissingParentCallCommonInspection
+     */
+    public function uriKey(): string
+    {
+        return 'anime-official-site-resource-lens';
+    }
+}
