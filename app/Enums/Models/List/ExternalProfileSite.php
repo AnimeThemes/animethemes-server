31cc76ef6d268011a94fe5b7216d411919ed1f76
Kyrch <kyrchk5@gmail.com>
2024-03-14 11:31:19 -0300

    feat: store external profile list action

--- app/Enums/Models/List/ExternalProfileSite.php
+++ app/Enums/Models/List/ExternalProfileSite.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Enums\Models\List;
 
 use App\Concerns\Enums\LocalizesName;
+use App\Enums\Models\Wiki\ResourceSite;
 
 /**
  * Enum ExternalProfileSite.
@@ -16,4 +17,20 @@ enum ExternalProfileSite: int
     case MAL = 0;
     case ANILIST = 1;
     case KITSU = 2;
+
+    /**
+     * Get the ResourceSite by the ExternalProfileSite value.
+     *
+     * @param  int|null  $value
+     * @return ResourceSite|null
+     */
+    public static function getResourceSite(?int $value): ?ResourceSite
+    {
+        return match ($value) {
+            static::MAL->value => ResourceSite::MAL,
+            static::ANILIST->value => ResourceSite::ANILIST,
+            static::KITSU->value => ResourceSite::KITSU,
+            default => null,
+        };
+    }
 }
