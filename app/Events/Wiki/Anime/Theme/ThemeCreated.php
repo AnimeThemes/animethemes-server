c7cbed03bf472e08aac9df529b02e6789fc6bdf3
Kyrch <kyrchk5@gmail.com>
2024-03-27 00:47:50 -0300

    feat: the first theme will be updated when creating a new theme (#642)
    * feat: the first theme will be updated  when creating a new theme
    
    * clean: removed the unused import

--- app/Events/Wiki/Anime/Theme/ThemeCreated.php
+++ app/Events/Wiki/Anime/Theme/ThemeCreated.php
@@ -34,6 +34,7 @@ class ThemeCreated extends WikiCreatedEvent implements UpdateRelatedIndicesEvent
     {
         parent::__construct($theme);
         $this->anime = $theme->anime;
+        $this->updateFirstTheme();
     }
 
     /**
@@ -69,5 +70,23 @@ class ThemeCreated extends WikiCreatedEvent implements UpdateRelatedIndicesEvent
             $entry->searchable();
             $entry->videos->each(fn (Video $video) => $video->searchable());
         });
+    }
+
+    /**
+     * Update the sequence attribute of the first theme when creating a new sequence theme.
+     * 
+     * @return void
+     */
+    protected function updateFirstTheme(): void
+    {
+        if ($this->getModel()->sequence >= 2) {
+            $this->anime->animethemes()->each(function (AnimeTheme $theme) {
+                if ($theme->type === $this->getModel()->type && $theme->sequence === null) {
+                    $theme->update([
+                        AnimeTheme::ATTRIBUTE_SEQUENCE => 1,
+                    ]);
+                }
+            });
+        }
     }
 }
