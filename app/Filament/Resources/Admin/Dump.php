c8c544c5820087e774454995465bd0201ccee1bf
Kyrch <kyrchk5@gmail.com>
2024-04-08 01:06:20 -0300

    fix(filament): the default sort method was in form

--- app/Filament/Resources/Admin/Dump.php
+++ app/Filament/Resources/Admin/Dump.php
@@ -113,7 +113,6 @@ class Dump extends BaseResource
                     ->rules(['required', 'max:192'])
                     ->hiddenOn(['create', 'edit']),
             ])
-            ->defaultSort(DumpModel::ATTRIBUTE_ID, 'desc')
             ->columns(1);
     }
 
@@ -140,6 +139,7 @@ class Dump extends BaseResource
                     ->searchable()
                     ->copyable(),
             ])
+            ->defaultSort(DumpModel::ATTRIBUTE_ID, 'desc')
             ->filters(static::getFilters())
             ->actions(static::getActions())
             ->bulkActions(static::getBulkActions());
