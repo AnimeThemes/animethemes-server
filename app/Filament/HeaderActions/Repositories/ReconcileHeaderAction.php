e544d0d28658512cd5cff29f8a0aaf1cc23f1172
Kyrch <kyrchk5@gmail.com>
2024-04-14 22:31:56 -0300

    style(filament): spaces

--- app/Filament/HeaderActions/Repositories/ReconcileHeaderAction.php
+++ app/Filament/HeaderActions/Repositories/ReconcileHeaderAction.php
@@ -14,35 +14,35 @@ use Illuminate\Database\Eloquent\Model;
  */
 abstract class ReconcileHeaderAction extends Action
 {
-  use ReconcilesRepositories;
+    use ReconcilesRepositories;
 
-  /**
-   * Initial setup for the action.
-   *
-   * @return void
-   */
-  protected function setUp(): void
-  {
-    parent::setUp();
+    /**
+     * Initial setup for the action.
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+      parent::setUp();
 
-    $this->action(fn (Model $record, array $data) => $this->handle($record, $data));
-  }
+      $this->action(fn (Model $record, array $data) => $this->handle($record, $data));
+    }
 
-  /**
-   * Perform the action on the given models.
-   *
-   * @param  Model  $record
-   * @param  array  $fields
-   * @return void
-   *
-   * @throws Exception
-   *
-   * @noinspection PhpUnusedParameterInspection
-   */
-  public function handle(Model $record, array $fields): void
-  {
-    $result = $this->reconcileRepositories($fields);
+    /**
+     * Perform the action on the given models.
+     *
+     * @param  Model  $record
+     * @param  array  $fields
+     * @return void
+     *
+     * @throws Exception
+     *
+     * @noinspection PhpUnusedParameterInspection
+     */
+    public function handle(Model $record, array $fields): void
+    {
+      $result = $this->reconcileRepositories($fields);
 
-    $result->toLog();
-  }
+      $result->toLog();
+    }
 }
