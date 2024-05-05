c834b39c5167d03419ddc38eb745de931ad1e967
Kyrch <kyrchk5@gmail.com>
2024-05-05 00:29:41 -0300

    feat: added livewire config

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,8 +25,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(PermissionSeeder::class);
         $this->call(RoleSeeder::class);
-        $this->call(VideoSeeder::class);
-        $this->call(AudioSeeder::class);
+     //   $this->call(VideoSeeder::class);
+     //   $this->call(AudioSeeder::class);
         $this->call(HashidsSeeder::class);
         $this->call(FeatureSeeder::class);
     }
