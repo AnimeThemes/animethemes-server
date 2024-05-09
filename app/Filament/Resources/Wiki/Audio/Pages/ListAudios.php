a2701c49b9b56cc53a4de22a8d91e63ae1387dd1
Kyrch <kyrchk5@gmail.com>
2024-05-09 15:07:04 -0300

    feat(filament): audio, resource & image tabs

--- app/Filament/Resources/Wiki/Audio/Pages/ListAudios.php
+++ app/Filament/Resources/Wiki/Audio/Pages/ListAudios.php
@@ -4,14 +4,19 @@ declare(strict_types=1);
 
 namespace App\Filament\Resources\Wiki\Audio\Pages;
 
+use App\Concerns\Filament\HasTabs;
 use App\Filament\Resources\Base\BaseListResources;
 use App\Filament\Resources\Wiki\Audio;
+use App\Filament\Tabs\Audio\AudioVideoTab;
+use Filament\Resources\Components\Tab;
 
 /**
  * Class ListAudios.
  */
 class ListAudios extends BaseListResources
 {
+    use HasTabs;
+
     protected static string $resource = Audio::class;
 
     /**
@@ -27,5 +32,17 @@ class ListAudios extends BaseListResources
             parent::getHeaderActions(),
             [],
         );
+    }
+
+    /**
+     * Get the tabs available.
+     *
+     * @return array
+     */
+    public function getTabs(): array
+    {
+        return ['all' => Tab::make()] + $this->toArray([
+            AudioVideoTab::class,
+        ]);
     }
 }
--- app/Filament/Resources/Wiki/ExternalResource/Pages/ListExternalResources.php
+++ app/Filament/Resources/Wiki/ExternalResource/Pages/ListExternalResources.php
@@ -4,14 +4,19 @@ declare(strict_types=1);
 
 namespace App\Filament\Resources\Wiki\ExternalResource\Pages;
 
+use App\Concerns\Filament\HasTabs;
 use App\Filament\Resources\Base\BaseListResources;
 use App\Filament\Resources\Wiki\ExternalResource;
+use App\Filament\Tabs\ExternalResource\ExternalResourceUnlinkedTab;
+use Filament\Resources\Components\Tab;
 
 /**
  * Class ListExternalResources.
  */
 class ListExternalResources extends BaseListResources
 {
+    use HasTabs;
+
     protected static string $resource = ExternalResource::class;
 
     /**
@@ -27,5 +32,17 @@ class ListExternalResources extends BaseListResources
             parent::getHeaderActions(),
             [],
         );
+    }
+
+    /**
+     * Get the tabs available.
+     *
+     * @return array
+     */
+    public function getTabs(): array
+    {
+        return ['all' => Tab::make()] + $this->toArray([
+            ExternalResourceUnlinkedTab::class,
+        ]);
     }
 }
