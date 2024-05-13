35d38b54e050061fcd0e5c3271a7aa407930591b
Kyrch <kyrchk5@gmail.com>
2024-05-13 18:23:34 -0300

    feat(filament): added dashboards & widgets (#674)

---
+++ app/Filament/Widgets/Wiki/Anime/AnimeChart.php
@@ -0,0 +1,78 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Filament\Widgets\Wiki\Anime;
+
+use App\Filament\Widgets\BaseChartWidget;
+use App\Models\Wiki\Anime;
+use Flowframe\Trend\TrendValue;
+
+/**
+ * Class AnimeChart.
+ */
+class AnimeChart extends BaseChartWidget
+{
+    /**
+     * Chart Id.
+     *
+     * @var string
+     */
+    protected static string $chartId = 'animeChart';
+
+    /**
+     * Get the displayed label of the widget.
+     *
+     * @return string
+     */
+    protected function getHeading(): string
+    {
+        return __('filament.resources.label.anime');
+    }
+
+    /**
+     * Chart options (series, labels, types, size, animations...)
+     * https://apexcharts.com/docs/options
+     *
+     * @return array
+     */
+    protected function getOptions(): array
+    {
+        $data = $this->perMonth(Anime::class);
+
+        return [
+            'chart' => [
+                'type' => 'area',
+                'height' => 300,
+            ],
+            'series' => [
+                [
+                    'name' => $this->getHeading(),
+                    'data' => $data->map(fn (TrendValue $value) => $value->aggregate),
+                ],
+            ],
+            'xaxis' => [
+                'categories' => $data->map(fn (TrendValue $value) => $this->translateDate($value->date)),
+                'labels' => [
+                    'style' => [
+                        'fontFamily' => 'inherit',
+                    ],
+                ],
+            ],
+            'yaxis' => [
+                'labels' => [
+                    'style' => [
+                        'fontFamily' => 'inherit',
+                    ],
+                ],
+            ],
+            'colors' => ['#71E9C5'],
+            'stroke' => [
+                'curve' => 'smooth',
+            ],
+            'dataLabels' => [
+                'enabled' => false,
+            ],
+        ];
+    }
+}
---
+++ app/Filament/Widgets/Wiki/Video/VideoChart.php
@@ -0,0 +1,78 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Filament\Widgets\Wiki\Video;
+
+use App\Filament\Widgets\BaseChartWidget;
+use App\Models\Wiki\Video;
+use Flowframe\Trend\TrendValue;
+
+/**
+ * Class VideoChart.
+ */
+class VideoChart extends BaseChartWidget
+{
+    /**
+     * Chart Id.
+     *
+     * @var string
+     */
+    protected static string $chartId = 'videoChart';
+
+    /**
+     * Get the displayed label of the widget.
+     *
+     * @return string
+     */
+    protected function getHeading(): string
+    {
+        return __('filament.resources.label.videos');
+    }
+
+    /**
+     * Chart options (series, labels, types, size, animations...)
+     * https://apexcharts.com/docs/options
+     *
+     * @return array
+     */
+    protected function getOptions(): array
+    {
+        $data = $this->perMonth(Video::class);
+
+        return [
+            'chart' => [
+                'type' => 'area',
+                'height' => 300,
+            ],
+            'series' => [
+                [
+                    'name' => $this->getHeading(),
+                    'data' => $data->map(fn (TrendValue $value) => $value->aggregate),
+                ],
+            ],
+            'xaxis' => [
+                'categories' => $data->map(fn (TrendValue $value) => $this->translateDate($value->date)),
+                'labels' => [
+                    'style' => [
+                        'fontFamily' => 'inherit',
+                    ],
+                ],
+            ],
+            'yaxis' => [
+                'labels' => [
+                    'style' => [
+                        'fontFamily' => 'inherit',
+                    ],
+                ],
+            ],
+            'colors' => ['#71E9C5'],
+            'stroke' => [
+                'curve' => 'smooth',
+            ],
+            'dataLabels' => [
+                'enabled' => false,
+            ],
+        ];
+    }
+}
