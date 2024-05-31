b603b6ccaaa947992baa40bceef45cdf9e94f609
Kyrch <kyrchk5@gmail.com>
2024-05-31 02:19:12 -0300

    refactor: discord bot integration

--- config/services.php
+++ config/services.php
@@ -35,15 +35,9 @@ return [
 
     'discord' => [
         'token' => env('DISCORD_BOT_API_TOKEN'),
+        'api_url' => env('DISCORD_BOT_API_URL'),
         'db_updates_discord_channel' => env('DB_UPDATES_DISCORD_CHANNEL'),
         'admin_discord_channel' => env('ADMIN_DISCORD_CHANNEL'),
-        'submissions_discord_channel' => env('SUBMISSIONS_DISCORD_CHANNEL'),
-        'submissions_forum_tags' => [
-            'winter' => env('WINTER_DISCORD_FORUM_TAG'),
-            'spring' => env('SPRING_DISCORD_FORUM_TAG'),
-            'summer' => env('SUMMER_DISCORD_FORUM_TAG'),
-            'fall' => env('FALL_DISCORD_FORUM_TAG')
-        ]
     ],
 
     'mal' => [
