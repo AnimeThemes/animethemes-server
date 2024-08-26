003a6a78af631df06cdcca0a2f337aff9b77127d
Kyrch <kyrchk5@gmail.com>
2024-08-25 23:48:31 -0300

    feat: external token model

---
+++ app/Events/List/ExternalProfile/ExternalToken/ExternalTokenCreated.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Events\List\ExternalProfile\ExternalToken;
+
+use App\Events\Base\List\ListCreatedEvent;
+use App\Models\List\External\ExternalToken;
+use App\Models\List\ExternalProfile;
+
+/**
+ * Class ExternalTokenCreated.
+ *
+ * @extends ListCreatedEvent<ExternalToken>
+ */
+class ExternalTokenCreated extends ListCreatedEvent
+{
+    /**
+     * The profile the token belongs to.
+     *
+     * @var ExternalProfile
+     */
+    protected ExternalProfile $profile;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param  ExternalToken  $token
+     */
+    public function __construct(ExternalToken $token)
+    {
+        parent::__construct($token);
+        $this->profile = $token->externalprofile;
+    }
+
+    /**
+     * Determine if the message should be sent.
+     *
+     * @return bool
+     *
+     * @noinspection PhpMissingParentCallCommonInspection
+     */
+    public function shouldSendDiscordMessage(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the model that has fired this event.
+     *
+     * @return ExternalToken
+     */
+    public function getModel(): ExternalToken
+    {
+        return $this->model;
+    }
+
+    /**
+     * Get the description for the Discord message payload.
+     *
+     * @return string
+     */
+    protected function getDiscordMessageDescription(): string
+    {
+        return "Token '**{$this->getModel()->getName()}**' has been created for External Profile '**{$this->profile->getName()}**'.";
+    }
+}
---
+++ app/Events/List/ExternalProfile/ExternalToken/ExternalTokenUpdated.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Events\List\ExternalProfile\ExternalToken;
+
+use App\Events\Base\List\ListUpdatedEvent;
+use App\Models\List\ExternalProfile;
+use App\Models\List\External\ExternalToken;
+
+/**
+ * Class ExternalTokenUpdated.
+ *
+ * @extends ListUpdatedEvent<ExternalToken>
+ */
+class ExternalTokenUpdated extends ListUpdatedEvent
+{
+    /**
+     * The profile the token belongs to.
+     *
+     * @var ExternalProfile
+     */
+    protected ExternalProfile $profile;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param  ExternalToken  $token
+     */
+    public function __construct(ExternalToken $token)
+    {
+        parent::__construct($token);
+        $this->profile = $token->externalprofile;
+        $this->initializeEmbedFields($token);
+    }
+
+    /**
+     * Determine if the message should be sent.
+     *
+     * @return bool
+     *
+     * @noinspection PhpMissingParentCallCommonInspection
+     */
+    public function shouldSendDiscordMessage(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the model that has fired this event.
+     *
+     * @return ExternalToken
+     */
+    public function getModel(): ExternalToken
+    {
+        return $this->model;
+    }
+
+    /**
+     * Get the description for the Discord message payload.
+     *
+     * @return string
+     */
+    protected function getDiscordMessageDescription(): string
+    {
+        return "Token '**{$this->getModel()->getName()}**' has been updated for External Profile '**{$this->profile->getName()}**'.";
+    }
+}
