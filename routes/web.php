f659baa947bce04f46b0265e16813a7d22d66f03
Kyrch <kyrchk5@gmail.com>
2024-10-25 02:07:04 -0300

    clean code

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,6 @@
 
 declare(strict_types=1);
 
-use App\Actions\Models\Wiki\BackfillSongAction;
-use App\Models\Wiki\Song;
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,9 +12,3 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Route::get('/test', function () {
-    $action = new BackfillSongAction(Song::find(2), 'https://claris.lnk.to/Hitorigoto');
-
-    $action->handle();
-
-});
