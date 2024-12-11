8f642d806b85407f40293742f5fd74f93aab6e17
Kyrch <kyrchk5@gmail.com>
2024-12-11 07:18:39 -0300

    feat: added new fields to views table & fixed filament edit action

---
+++ app/Models/Service/View.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models\Service;
+
+use App\Models\Auth\User;
+use CyrildeWit\EloquentViewable\View as BaseView;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Request;
+
+/**
+ * Class View.
+ *
+ * @property string|null $referer
+ * @property string|null $user_agent
+ * @property int|null $user_id
+ * @property User|null $user
+ */
+class View extends BaseView
+{
+    /**
+     * Bootstrap the model.
+     *
+     * @return void
+     */
+    protected static function boot(): void
+    {
+        parent::boot();
+
+        static::creating(function (View $view) {
+            $view->user_agent = Request::userAgent();
+            $view->user_id = Auth::id();
+            $view->referer = Request::header('referer');
+        });
+    }
+
+    /**
+     * Get the user related to the view.
+     *
+     * @return BelongsTo<User, $this>
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
