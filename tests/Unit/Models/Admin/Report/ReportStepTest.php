5e792a336be020430c97a91e9338bca01c660d67
Kyrch <kyrchk5@gmail.com>
2024-12-27 23:11:31 -0300

    tests: added missing unit tests

--- tests/Unit/Models/Admin/Report/ReportStepTest.php
+++ tests/Unit/Models/Admin/Report/ReportStepTest.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace Tests\Unit\Models\Admin;
 
 use App\Enums\Models\Admin\ApprovableStatus;
+use App\Enums\Models\Admin\ReportActionType;
 use App\Models\Admin\Report;
 use App\Models\Admin\Report\ReportStep;
+use App\Models\Wiki\Anime;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Carbon;
@@ -26,7 +28,9 @@ class ReportStepTest extends TestCase
      */
     public function testNameable(): void
     {
-        $step = ReportStep::factory()->createOne();
+        $step = ReportStep::factory()
+            ->for(Report::factory())
+            ->createOne();
 
         static::assertIsString($step->getName());
     }
@@ -38,9 +42,41 @@ class ReportStepTest extends TestCase
      */
     public function testHasSubtitle(): void
     {
-        $step = ReportStep::factory()->createOne();
+        $step = ReportStep::factory()
+            ->for(Report::factory())
+            ->createOne();
 
         static::assertIsString($step->getSubtitle());
+    }
+
+    /**
+     * The action attribute of a step shall be cast to an ReportActionType enum instance.
+     *
+     * @return void
+     */
+    public function testCastsActionToEnum(): void
+    {
+        $step = ReportStep::factory()
+            ->for(Report::factory())
+            ->createOne();
+
+        static::assertInstanceOf(ReportActionType::class, $step->action);
+    }
+
+    /**
+     * The action attribute of a step shall be cast to an array.
+     *
+     * @return void
+     */
+    public function testCastsFieldsToArray(): void
+    {
+        $anime = Anime::factory()->makeOne();
+
+        $step = ReportStep::factory()
+            ->for(Report::factory())
+            ->createOne([ReportStep::ATTRIBUTE_FIELDS => $anime->attributesToArray()]);
+
+        static::assertIsArray($step->fields);
     }
 
     /**
@@ -50,7 +86,9 @@ class ReportStepTest extends TestCase
      */
     public function testCastsFinishedAt(): void
     {
-        $step = ReportStep::factory()->createOne([ReportStep::ATTRIBUTE_FINISHED_AT => now()]);
+        $step = ReportStep::factory()
+            ->for(Report::factory())
+            ->createOne([ReportStep::ATTRIBUTE_FINISHED_AT => now()]);
 
         static::assertInstanceOf(Carbon::class, $step->finished_at);
     }
@@ -62,7 +100,9 @@ class ReportStepTest extends TestCase
      */
     public function testCastsStatusToEnum(): void
     {
-        $step = ReportStep::factory()->createOne();
+        $step = ReportStep::factory()
+            ->for(Report::factory())
+            ->createOne();
 
         static::assertInstanceOf(ApprovableStatus::class, $step->status);
     }
