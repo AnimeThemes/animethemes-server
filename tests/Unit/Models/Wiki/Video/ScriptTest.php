2f6a8ecb8067e01259c0d5ec27c836583a9115df
Kyrch <kyrchk5@gmail.com>
2025-01-06 17:01:13 -0300

    tests: added missing script test

--- tests/Unit/Models/Wiki/Video/ScriptTest.php
+++ tests/Unit/Models/Wiki/Video/ScriptTest.php
@@ -4,9 +4,16 @@ declare(strict_types=1);
 
 namespace Tests\Unit\Models\Wiki\Video;
 
+use App\Constants\Config\VideoConstants;
+use App\Events\Wiki\Video\Script\VideoScriptForceDeleting;
 use App\Models\Wiki\Video;
 use App\Models\Wiki\Video\VideoScript;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\Testing\File;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 
 /**
@@ -14,6 +21,8 @@ use Tests\TestCase;
  */
 class ScriptTest extends TestCase
 {
+    use WithFaker;
+
     /**
      * Scripts shall be nameable.
      *
@@ -53,5 +62,47 @@ class ScriptTest extends TestCase
 
         static::assertInstanceOf(BelongsTo::class, $script->video());
         static::assertInstanceOf(Video::class, $script->video()->first());
+    }
+
+    /**
+     * The script shall not be deleted from storage when the VideoScript is deleted.
+     *
+     * @return void
+     */
+    public function testScriptStorageDeletion(): void
+    {
+        $fs = Storage::fake(Config::get(VideoConstants::SCRIPT_DISK_QUALIFIED));
+        $file = File::fake()->create($this->faker->word().'.ogg', $this->faker->randomDigitNotNull());
+        $fsFile = $fs->putFile('', $file);
+
+        $script = VideoScript::factory()->createOne([
+            VideoScript::ATTRIBUTE_PATH => $fsFile,
+        ]);
+
+        $script->delete();
+
+        static::assertTrue($fs->exists($script->path));
+    }
+
+    /**
+     * The script shall be deleted from storage when the VideoScript is force deleted.
+     *
+     * @return void
+     */
+    public function testScriptStorageForceDeletion(): void
+    {
+        Event::fakeExcept(VideoScriptForceDeleting::class);
+
+        $fs = Storage::fake(Config::get(VideoConstants::SCRIPT_DISK_QUALIFIED));
+        $file = File::fake()->create($this->faker->word().'.ogg', $this->faker->randomDigitNotNull());
+        $fsFile = $fs->putFile('', $file);
+
+        $script = VideoScript::factory()->createOne([
+            VideoScript::ATTRIBUTE_PATH => $fsFile,
+        ]);
+
+        $script->forceDelete();
+
+        static::assertFalse($fs->exists($script->path));
     }
 }
