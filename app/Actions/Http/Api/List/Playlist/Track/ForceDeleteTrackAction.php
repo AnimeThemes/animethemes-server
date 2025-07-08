3440830adc0a1f4da22544e9bb8ca10c3752f34e
Kyrch <kyrchk5@gmail.com>
2025-07-08 04:43:17 -0300

    fix: 2nd attempt to fix playlist race conditions (#858)

--- app/Actions/Http/Api/List/Playlist/Track/ForceDeleteTrackAction.php
+++ app/Actions/Http/Api/List/Playlist/Track/ForceDeleteTrackAction.php
@@ -9,6 +9,7 @@ use App\Actions\Models\List\Playlist\RemoveTrackAction;
 use App\Models\List\Playlist;
 use App\Models\List\Playlist\PlaylistTrack;
 use Exception;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
@@ -32,18 +33,22 @@ class ForceDeleteTrackAction
             DB::beginTransaction();
 
             // Lock tracks to prevent race conditions.
-            Playlist::query()->whereKey($playlist->getKey())->lockForUpdate()->first();
-            $playlist->tracks()->getQuery()->lockForUpdate()->count();
+            $message = Cache::lock('playlist-lock'.$playlist->getKey(), 10)->block(30, function () use ($playlist, $track) {
+                Playlist::query()->whereKey($playlist->getKey())->lockForUpdate()->first();
+                $playlist->tracks()->getQuery()->lockForUpdate()->count();
 
-            $removeAction = new RemoveTrackAction();
+                $removeAction = new RemoveTrackAction();
 
-            $removeAction->remove($playlist, $track);
+                $removeAction->remove($playlist, $track);
 
-            $forceDeleteAction = new ForceDeleteAction();
+                $forceDeleteAction = new ForceDeleteAction();
 
-            $message = $forceDeleteAction->forceDelete($track);
+                $message = $forceDeleteAction->forceDelete($track);
 
-            DB::commit();
+                DB::commit();
+
+                return $message;
+            });
 
             return $message;
         } catch (Exception $e) {
