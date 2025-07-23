d9b87058f3dec58aba051bce960a263fa87dc108
Kyrch <kyrchk5@gmail.com>
2025-07-23 20:51:13 -0300

    docs: removed unnecessary docs from app/scopes

--- app/Scopes/WithoutInsertSongScope.php
+++ app/Scopes/WithoutInsertSongScope.php
@@ -11,17 +11,12 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Scope;
 
-/**
- * Class WithoutInsertSongScope.
- */
 class WithoutInsertSongScope implements Scope
 {
     /**
      * Apply the scope to a given Eloquent query builder.
      *
      * @param  Builder  $builder
-     * @param  Model  $model
-     * @return void
      */
     public function apply(Builder $builder, Model $model): void
     {
