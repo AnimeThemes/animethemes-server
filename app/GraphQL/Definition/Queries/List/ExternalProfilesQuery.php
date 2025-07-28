1b72050cda421566c378fea042a505c85e9a70c8
Kyrch <kyrchk5@gmail.com>
2025-07-28 19:09:10 -0300

    feat(graphql): random sorting (#896)

--- app/GraphQL/Definition/Queries/List/ExternalProfilesQuery.php
+++ app/GraphQL/Definition/Queries/List/ExternalProfilesQuery.php
@@ -7,7 +7,6 @@ namespace App\GraphQL\Definition\Queries\List;
 use App\GraphQL\Attributes\Resolvers\UseBuilderDirective;
 use App\GraphQL\Attributes\Resolvers\UsePaginateDirective;
 use App\GraphQL\Attributes\UseSearchDirective;
-use App\GraphQL\Builders\List\ExternalProfileBuilder;
 use App\GraphQL\Controllers\List\ExternalProfileController;
 use App\GraphQL\Definition\Queries\EloquentQuery;
 use App\GraphQL\Definition\Types\List\ExternalProfileType;
