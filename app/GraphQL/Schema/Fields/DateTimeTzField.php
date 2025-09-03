08dfb5b099c17bb87b271ab2b9bc73071e1a6043
Kyrch <kyrchk5@gmail.com>
2025-09-03 16:17:13 -0300

    feat(graphql): added format argument for date fields (#953)

--- app/GraphQL/Schema/Fields/DateTimeTzField.php
+++ app/GraphQL/Schema/Fields/DateTimeTzField.php
@@ -4,10 +4,15 @@ declare(strict_types=1);
 
 namespace App\GraphQL\Schema\Fields;
 
+use App\GraphQL\Support\Argument\Argument;
 use App\GraphQL\Support\Filter\EqFilter;
 use App\GraphQL\Support\Filter\Filter;
 use App\GraphQL\Support\Filter\GreaterFilter;
 use App\GraphQL\Support\Filter\LesserFilter;
+use GraphQL\Type\Definition\ResolveInfo;
+use GraphQL\Type\Definition\Type;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Carbon;
 
 abstract class DateTimeTzField extends StringField
 {
@@ -21,5 +26,29 @@ abstract class DateTimeTzField extends StringField
             new LesserFilter($this),
             new GreaterFilter($this),
         ];
+    }
+
+    /**
+     * The arguments of the type.
+     *
+     * @return Argument[]
+     */
+    public function arguments(): array
+    {
+        return [
+            new Argument('format', Type::string())
+                ->required()
+                ->withDefaultValue('Y-m-d H:i:s'),
+        ];
+    }
+
+    public function resolve($root, array $args, $context, ResolveInfo $resolveInfo): mixed
+    {
+        $format = Arr::get($args, 'format');
+
+        /** @var Carbon $field */
+        $field = $root->{$this->getColumn()};
+
+        return $field->format($format);
     }
 }
