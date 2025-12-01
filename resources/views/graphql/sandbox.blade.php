95457825a602b243ddbb4ff51c5bdff7f5d59922
Kyrch <kyrchk5@gmail.com>
2025-12-01 15:52:47 -0300

    style: fix sandbox margin

--- resources/views/graphql/sandbox.blade.php
+++ resources/views/graphql/sandbox.blade.php
@@ -1,8 +1,10 @@
-<div style="width: 100%; height: 100%;" id='embedded-sandbox'></div>
-<script src="https://embeddable-sandbox.cdn.apollographql.com/v2/embeddable-sandbox.umd.production.min.js"></script> 
-<script>
-    new window.EmbeddedSandbox({
-        target: '#embedded-sandbox',
-        initialEndpoint: '{{ $endpoint }}',
-    });
-</script>
+<body style="margin: 0;">
+    <div style="width: 100%; height: 100%;" id='embedded-sandbox'></div>
+    <script src="https://embeddable-sandbox.cdn.apollographql.com/v2/embeddable-sandbox.umd.production.min.js"></script> 
+    <script>
+        new window.EmbeddedSandbox({
+            target: '#embedded-sandbox',
+            initialEndpoint: '{{ $endpoint }}',
+        });
+    </script>
+</body>
