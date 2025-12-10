51226fe049e2cbabf1bae669231f6741024fa1e1
Kyrch <kyrchk5@gmail.com>
2025-12-10 18:53:32 -0300

    feat(submissions): notes field

--- lang/en/submissions.php
+++ lang/en/submissions.php
@@ -3,6 +3,14 @@
 declare(strict_types=1);
 
 return [
+    'fields' => [
+        'base' => [
+            'notes' => [
+                'help' => 'Add any relevant notes or sources that support this submission.',
+                'name' => 'Notes / Sources',
+            ],
+        ],
+    ],
     'resources' => [
         'label' => [
             'anime' => 'Anime',
