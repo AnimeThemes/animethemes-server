34bb043857bf3608fefcd9b35440242c75f7c882
Kyrch <kyrchk5@gmail.com>
2026-02-08 01:26:44 -0300

    refactor: lazy load attributes on closures

--- app/Http/Api/Field/List/Playlist/PlaylistDescriptionField.php
+++ app/Http/Api/Field/List/Playlist/PlaylistDescriptionField.php
@@ -9,7 +9,6 @@ use App\Contracts\Http\Api\Field\UpdatableField;
 use App\Http\Api\Field\StringField;
 use App\Http\Api\Schema\Schema;
 use App\Models\List\Playlist;
-use App\Rules\ModerationRule;
 use Illuminate\Http\Request;
 
 class PlaylistDescriptionField extends StringField implements CreatableField, UpdatableField
