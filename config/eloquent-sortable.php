b0a617a8e64684dcd08c3586fead7355a05bed46
Kyrch <kyrchk5@gmail.com>
2026-02-27 21:05:02 -0300

    feat: add tracks position

---
+++ config/eloquent-sortable.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    /*
+     * Which column will be used as the order column.
+     */
+    'order_column_name' => 'position',
+
+    /*
+     * Define if the models should sort when creating.
+     * When true, the package will automatically assign the highest order number to a new model
+     */
+    'sort_when_creating' => true,
+
+    /*
+     * Define if the timestamps should be ignored when sorting.
+     * When true, updated_at will not be updated when using setNewOrder
+     */
+    'ignore_timestamps' => false,
+];
---
+++ database/migrations/2026_02_27_102432_add_position_to_playlist_tracks_table.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        if (! Schema::hasColumn('playlist_tracks', 'position')) {
+            Schema::table('playlist_tracks', function (Blueprint $table) {
+                $table->integer('position')->default(1);
+            });
+        }
+    }
+};
