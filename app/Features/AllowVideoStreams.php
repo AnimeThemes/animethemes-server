107e01ae76a4ee0dede58cb8f62cc8a2062f3919
Kyrch <kyrchk5@gmail.com>
2026-03-05 09:10:34 -0300

    fix: fix infinite recursion in feature classes

--- app/Features/AllowVideoStreams.php
+++ app/Features/AllowVideoStreams.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 
 namespace App\Features;
 
+use App\Constants\FeatureConstants;
 use App\Enums\Auth\SpecialPermission;
+use App\Models\Admin\Feature;
 use App\Models\Auth\User;
-use Laravel\Pennant\Feature;
 
 class AllowVideoStreams
 {
@@ -16,6 +17,9 @@ class AllowVideoStreams
             return true;
         }
 
-        return Feature::for(null)->value(static::class);
+        return (bool) Feature::query()
+            ->where(Feature::ATTRIBUTE_SCOPE, FeatureConstants::NULL_SCOPE)
+            ->where(Feature::ATTRIBUTE_NAME, static::class)
+            ->value(Feature::ATTRIBUTE_VALUE);
     }
 }
