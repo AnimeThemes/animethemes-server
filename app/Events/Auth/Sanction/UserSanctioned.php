262ae31103bf2de546517c300c1bc068424c2b2c
Kyrch <kyrchk5@gmail.com>
2026-03-25 16:46:44 -0300

    refine prohibition and sanction events

--- app/Events/Auth/Sanction/UserSanctioned.php
+++ app/Events/Auth/Sanction/UserSanctioned.php
@@ -2,44 +2,41 @@
 
 declare(strict_types=1);
 
-namespace App\Events\Auth\Prohibition;
+namespace App\Events\Auth\Sanction;
 
 use App\Constants\Config\ServiceConstants;
 use App\Contracts\Events\DiscordMessageEvent;
 use App\Contracts\Events\NotifiesUsersEvent;
 use App\Enums\Discord\EmbedColor;
-use App\Models\Auth\Prohibition;
+use App\Events\BaseEvent;
+use App\Models\Auth\Sanction;
 use App\Models\Auth\User;
 use App\Notifications\Auth\ProhibitionOrSanctionNotification;
 use DateTimeInterface;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Config;
 use NotificationChannels\Discord\DiscordMessage;
 
-class ModelProhibited implements DiscordMessageEvent, NotifiesUsersEvent
+class UserSanctioned extends BaseEvent implements DiscordMessageEvent, NotifiesUsersEvent
 {
     use Dispatchable;
     use SerializesModels;
 
-    /**
-     * @param  User  $model
-     * @param  Prohibition  $prohibition
-     * @param  User  $moderator
-     */
     public function __construct(
-        public Model $model,
-        public mixed $prohibition,
+        public User $user,
+        public Sanction $sanction,
         public ?DateTimeInterface $expiresAt = null,
         public ?string $reason = null,
-        public ?Model $moderator = null,
-    ) {}
+        public ?User $moderator = null,
+    ) {
+        parent::__construct($user);
+    }
 
     public function getDiscordMessage(): DiscordMessage
     {
         return DiscordMessage::create('', [
-            'description' => "Prohibition '**{$this->prohibition->name}**' triggered for user '**{$this->model->getName()}**'. Reason: {$this->reason}",
+            'description' => "Sanction '**{$this->sanction->name}**' triggered for user '**{$this->user->getName()}**'. Reason: {$this->reason}",
             'color' => EmbedColor::GREEN->value,
         ]);
     }
@@ -56,6 +53,6 @@ class ModelProhibited implements DiscordMessageEvent, NotifiesUsersEvent
 
     public function notify(): void
     {
-        $this->model->notify(new ProhibitionOrSanctionNotification($this->prohibition, $this->reason, $this->expiresAt));
+        $this->user->notify(new ProhibitionOrSanctionNotification($this->sanction, $this->reason, $this->expiresAt));
     }
 }
