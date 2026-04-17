1711f62d3bda9f3aaf631ab2025f5eb80a30b3fa
Kyrch <kyrchk5@gmail.com>
2026-04-17 10:39:46 -0300

    feat(graphql): deprecate deletedAt field

--- app/GraphQL/Schema/Fields/Base/DeletedAtField.php
+++ app/GraphQL/Schema/Fields/Base/DeletedAtField.php
@@ -5,9 +5,10 @@ declare(strict_types=1);
 namespace App\GraphQL\Schema\Fields\Base;
 
 use App\Constants\ModelConstants;
+use App\Contracts\GraphQL\Fields\DeprecatedField;
 use App\GraphQL\Schema\Fields\DateTimeTzField;
 
-class DeletedAtField extends DateTimeTzField
+class DeletedAtField extends DateTimeTzField implements DeprecatedField
 {
     public function __construct()
     {
@@ -17,5 +18,10 @@ class DeletedAtField extends DateTimeTzField
     public function description(): string
     {
         return 'The date that the resource was deleted';
+    }
+
+    public function deprecationReason(): string
+    {
+        return 'It\'ll be removed alongside its related filters as this field is for internal use only';
     }
 }
